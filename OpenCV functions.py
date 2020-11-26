ced78fc9950748d884a49ea59a5d728c70a732af
DenisaAdam <adamdenisa97@yahoo.com>
2020-11-26 11:26:18 +0200

    Add files via upload

---
+++ OpenCV functions.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 24 15:46:05 2020
+
+@author: Denisa
+"""
+
+from __future__ import print_function
+
+import numpy as np
+import cv2
+
+ply_header = '''ply
+format ascii 1.0
+element vertex %(vert_num)d
+property float x
+property float y
+property float z
+property uchar red
+property uchar green
+property uchar blue
+end_header
+'''
+
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
+def write_ply(fn, verts, colors):
+    verts = verts.reshape(-1, 3)
+    colors = colors.reshape(-1, 3)
+    verts = np.hstack([verts, colors])
+    with open(fn, 'wb') as f:
+        f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
+        np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
+
+
+if __name__ == '__main__':
+    print('loading images...')
+    imgL = ResizeWithAspectRatio(cv2.pyrDown( cv2.imread('im2.png') ), width=800)  # downscale images for faster processing
+    imgR = ResizeWithAspectRatio(cv2.pyrDown( cv2.imread('im3.png') ), width=800)
+
+    # disparity range is tuned for 'aloe' image pair
+    window_size =5
+    min_disp = 32
+    num_disp = 112-min_disp
+    stereo = cv2.StereoSGBM_create(minDisparity = min_disp,
+        numDisparities = num_disp,
+        blockSize = 10,
+        P1 = 8*3*window_size**2,
+        P2 = 32*3*window_size**2,
+        disp12MaxDiff = 1,
+        uniquenessRatio = 10,
+        speckleWindowSize = 100,
+        speckleRange = 32
+    )
+
+    print('computing disparity...')
+    disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
+
+    print('generating 3d point cloud...',)
+    h, w = imgL.shape[:2]
+    f = 0.8*w                          # guess for focal length
+    Q = np.float32([[1, 0, 0, -0.5*w],
+                    [0,-1, 0,  0.5*h], # turn points 180 deg around x-axis,
+                    [0, 0, 0,     -f], # so that y-axis looks up
+                    [0, 0, 1,      0]])
+    points = cv2.reprojectImageTo3D(disp, Q)
+    colors = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
+    mask = disp > disp.min()
+    out_points = points[mask]
+    out_colors = colors[mask]
+    out_fn = 'out.ply'
+    write_ply('out.ply', out_points, out_colors)
+    print('%s saved' % 'out.ply')
+
+    cv2.imshow('left', imgL)
+    cv2.imshow('disparity', (disp-min_disp)/num_disp)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
